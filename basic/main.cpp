76cde6540ccc35233d26b3e5df9d46c41eea83ff
Freddylo <freddy_luo@moneynet.tw>
2022-05-28 01:30:24 +0800

    feature: dynamic memory allocation

--- basic/main.cpp
+++ basic/main.cpp
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "cstdlib"
 
 int pointer(){
     int i, *pi;
@@ -11,6 +12,19 @@ int pointer(){
     return 0;
 }
 
+int dynamicMemoryAllocation(){
+    int i, *pi;
+    float f, *pf;
+    pi = (int *) malloc(sizeof (int));
+    pf = (float *) malloc(sizeof (float ));
+    *pi = 1024;
+    *pf = 3.14;
+    printf("an integer = %d, a float = %f\n", *pi, *pf);
+    free(pi);
+    free(pf);
+}
+
 int main() {
-    pointer();
+//    pointer();
+dynamicMemoryAllocation();
 }
